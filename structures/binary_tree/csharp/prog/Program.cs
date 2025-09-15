ff6332fff34dfc8d004fd2538869bfa4c48319d2
Дмитрий <demon04_08@list.ru>
2025-09-15 21:50:05 +0300

    Реализация bst на c#

---
+++ structures/binary_tree/csharp/prog/Program.cs
@@ -0,0 +1,30 @@
+﻿using tree;
+
+
+
+
+tree.BST<int, int> bst = new BST<int, int>(6, 111);
+bst.Insert(4, 33);
+bst.Insert(2, 33);
+bst.Insert(1, 33);
+bst.Insert(3, 33);
+
+var visit = (int key, int data) => Console.WriteLine(key.ToString(), data.ToString());
+
+bst.PreOrderRecursive(bst, visit);
+
+Console.Write("\n");
+bst = bst.RemoveNode(bst, 1)!;
+bst.PreOrderRecursive(bst, visit);
+
+Console.Write("\n");
+bst = bst.RemoveNode(bst, 2)!;
+bst.PreOrderRecursive(bst, visit);
+
+Console.Write("\n");
+bst = bst.RemoveNode(bst, 6)!;
+bst.PreOrderRecursive(bst, visit);
+
+Console.Write("\n");
+bst = bst.RemoveNode(bst, 3)!;
+bst.PreOrderRecursive(bst, visit);
