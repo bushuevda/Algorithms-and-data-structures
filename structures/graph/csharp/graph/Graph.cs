3c6c78ebe4f076388a8d0df0c4a5a4de858c8586
Дмитрий <demon04_08@list.ru>
2025-09-19 20:45:35 +0300

    Наработки по graph

---
+++ structures/graph/csharp/graph/Graph.cs
@@ -0,0 +1,31 @@
+﻿namespace graph;
+
+
+
+
+public class Graph
+{
+
+    public List<List<int>> adjasentList_;
+
+
+    public Graph(){
+        adjasentList_ = new List<List<int>>();
+    }
+
+    public void AddNode(List<int> adjasentList){
+        adjasentList_.Add(adjasentList);
+    }
+
+    public void AddWeight(int node_from, int node_to, int weight){
+
+        if(adjasentList_[node_from].FindLast((val) => val == node_to) == 0){
+            adjasentList_[node_from].Add(node_to);
+            adjasentList_[node_from][node_to] = weight;
+        }
+    }
+
+    public void SetWeight(int node_from, int node_to, int weight){
+        adjasentList_[node_from][node_to] = weight;
+    }
+}
