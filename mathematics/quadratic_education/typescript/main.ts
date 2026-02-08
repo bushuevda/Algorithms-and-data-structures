284032a2fb65628ee97f4b4c8d2a3257e3e5e85b
Дмитрий <demon04_08@list.ru>
2026-02-08 11:42:53 +0300

    поменял назв

--- mathematics/quadratic_education/typescript/main.ts
+++
@@ -1,4 +0,0 @@
-import { QuadraticEquationUtil, QuadraticEquation } from "./quadratic_education";
-
-
-console.log(QuadraticEquationUtil.calculate({a: -1, b: 6, c: 7}))
---
+++ mathematics/quadratic_equation/typescript/main.ts
@@ -0,0 +1,4 @@
+import { QuadraticEquationUtil, QuadraticEquation } from "./quadratic_equation";
+
+
+console.log(QuadraticEquationUtil.calculate({a: -1, b: 6, c: 7}))
