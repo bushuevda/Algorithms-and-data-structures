91096f193c8722cb806b5913df127b6eb3767109
Дмитрий <demon04_08@list.ru>
2026-02-16 18:38:46 +0300

    Правки

--- structures/binary_tree/cpp/bst/bst.hpp
+++ structures/binary_tree/cpp/bst/bst.hpp
@@ -12,16 +12,14 @@ public:
     Bst <T, K>* parent_;
     std::unique_ptr<Bst <T, K>> left_ = nullptr;
     std::unique_ptr<Bst <T, K>> right_ = nullptr;
-    Bst <T, K>(T key, K data, Bst<T,K>* parent = nullptr): key_(key), data_(data), parent_(parent){
-    };
+    Bst <T, K>(T key, K data, Bst<T,K>* parent = nullptr): key_(key), data_(data), parent_(parent){};
     
     void insert(T key, K data){
     if(this->key_ < key){
         if(!this->left_){
             this->left_ = std::unique_ptr<Bst<T, K>>(new Bst<T, K>(std::move(key), std::move(data)));
-            this->left_ = 4;
         } else {
-            
+            this->left_->insert(key, data);
         }
 
     }
